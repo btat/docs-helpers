95964162878b2d9ba07f289d2c2b4bc6aca3fb55
Billy Tat <btat@suse.com>
2024-09-09 17:19:36 -0700

    Add asset status checker

---
+++ unused_assets.rb
@@ -0,0 +1,24 @@
+require 'json'
+
+# ARGV[0] = asset directory
+# ARGV[1] = search directory
+# ARGV[2] = "delete" to also remove unused asset file
+
+asset_dir =  ARGV[0]
+search_dir = ARGV[1]
+
+results = {"Used" => [], "Unused" => []}
+
+Dir.glob("#{asset_dir.chomp("/")}/**/*.*") do |asset_full_path|
+  asset = asset_full_path.sub(asset_dir,"")
+  if !%x[ grep -ri #{asset} #{search_dir} ].empty?
+    results["Used"] << asset
+  else
+    results["Unused"] << asset
+    if ARGV[2] == "delete"
+      File.delete(asset_full_path)
+    end
+  end
+end
+
+File.write("unused_assets.json", JSON.pretty_generate(results))
